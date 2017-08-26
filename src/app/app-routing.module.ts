c24f8e41a24e0285ba5006f58cd9177eeb2f327d
Angular4-Traning <small_chung49@hotmail.com>
2017-08-26 17:42:25 +0800

    modify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Route, Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { fallbackRoute } from "./Shared/fallback-route";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardsComponent } from './cards/cards.component';
+import { FlotComponent } from './charts/flot/flot.component';
+
+
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  {path:'', redirectTo:'/dashboard', pathMatch:'full'},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'cards', component:CardsComponent},
+  {path:'charts',
+    children:[
+      {path:'flot', component:FlotComponent}
+    ]},
+  fallbackRoute
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{enableTracing:true, useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,10 @@
+
 /* SystemJS module definition */
 declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+declare var $:any;
+declare var Colors: any;
+
